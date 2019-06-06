95f9896663d6f7e18e42d2bd394a34a2117d98ba
Davis Goodin <dagood@microsoft.com>
2019-06-06 14:12:57 -0500

    Delete some old infra, use Arcade in build scripts

--- build.cmd
+++ build.cmd
@@ -1,2 +1,15 @@
-@call %~dp0run.cmd build %*
-@exit /b %ERRORLEVEL%
+@echo off
+
+if "%~1"=="-h" goto help
+if "%~1"=="-help" goto help
+if "%~1"=="-?" goto help
+if "%~1"=="/?" goto help
+
+powershell -ExecutionPolicy ByPass -NoProfile -File "%~dp0eng\build.ps1" %*
+goto end
+
+:help
+powershell -ExecutionPolicy ByPass -NoProfile -Command "& { . %~dp0eng\build.ps1; Get-Help }"
+
+:end
+exit /b %ERRORLEVEL%
